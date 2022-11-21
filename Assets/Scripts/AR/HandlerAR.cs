b620d44522db9c1a5ed9ce99d3c31b0bd811fa54
Erick Hermawan <erick.hermawan21@gmail.com>
2022-11-21 10:27:33 +0800

    Checkpoint semua AR jalan

--- Assets/Scripts/AR/HandlerAR.cs
+++ Assets/Scripts/AR/HandlerAR.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.IO;
 using UnityEngine;
@@ -9,7 +8,7 @@ public class HandlerAR : MonoBehaviour
 {
     [SerializeField] Image _screenshot;
     [SerializeField] GameObject _topContainer, _bottomContainer;
-    readonly string IMAGE_NAME = "borobudur-ar.png";
+    //readonly string IMAGE_NAME = Application.persistentDataPath + "/borobudur-ar.png";
 
     public void CloseApp()
     {
@@ -32,33 +31,34 @@ public class HandlerAR : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         // Take screenshot
-        ScreenCapture.CaptureScreenshot(IMAGE_NAME);
+        //ScreenCapture.CaptureScreenshot(IMAGE_NAME);
+        Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
 
         // Show UI after we're done
         _topContainer.SetActive(true); _bottomContainer.SetActive(true);
 
         // Show on Image UI
         yield return new WaitForSeconds(1);
-        Texture2D texture = LoadPNG(IMAGE_NAME);
+        //Texture2D texture = LoadPNG(IMAGE_NAME);
         _screenshot.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
         _screenshot.gameObject.SetActive(true);
     }
 
-    public static Texture2D LoadPNG(string filePath)
-    {
-        Texture2D texture = null;
-        byte[] fileData;
+    //public static Texture2D LoadPNG(string filePath)
+    //{
+    //    Texture2D texture = null;
+    //    byte[] fileData;
 
-        if (File.Exists(filePath))
-        {
-            fileData = File.ReadAllBytes(filePath);
-            texture = new Texture2D(2, 2);
-            if (texture.LoadImage(fileData))  // Load the imagedata into the texture (size is set automatically)
-                return texture;
-            texture.LoadImage(fileData); //..this will auto-resize the texture dimensions.
-        }
-        return null;
-    }
+    //    if (File.Exists(filePath))
+    //    {
+    //        fileData = File.ReadAllBytes(filePath);
+    //        texture = new Texture2D(2, 2);
+    //        if (texture.LoadImage(fileData))  // Load the imagedata into the texture (size is set automatically)
+    //            return texture;
+    //        texture.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+    //    }
+    //    return null;
+    //}
 
     public void DoRetake()
     {
